e47585cce57c1b9af1e97dbe7e82193b034b9e27
Ronald Kassab <66827298+kassabronald@users.noreply.github.com>
2023-04-23 20:21:20 +0300

    Fixed an endpoint

--- apis/fluctuations.py
+++ apis/fluctuations.py
@@ -1,14 +1,14 @@
 from flask import Blueprint, request, jsonify
 import datetime
 from services.utils import getExchangeRates
-from services.fluctuationResponse import fluctuationResponseUsdToLbp, fluctuationResponseLbpToUsd
+from services.fluctuationResponse import FluctuationResponse
 
 
 fluctuations = Blueprint('fluctuations', __name__,
                          url_prefix='/fluctuations')
 
 
-@fluctuations.route('/usd-to-lbp', methods=['GET'], strict_slashes=False)
+@fluctuations.route('/', methods=['GET'], strict_slashes=False)
 def get_exchangeRateFluctuationsUSD():
     args = request.args
     startYear = int(args.get('startYear'))
@@ -40,49 +40,9 @@ def get_exchangeRateFluctuationsUSD():
         if (lbp_to_usd_rate == 0):
             lbp_to_usd_rate = "No Data Available"
 
-        FluctuationResponseUsdToLbp = fluctuationResponseUsdToLbp(
-            start_of_period, end_of_period, usd_to_lbp_rate)
-        fluctuations.append(FluctuationResponseUsdToLbp.serialize())
-        current_date += datetime.timedelta(days=1)
-
-    return jsonify(fluctuations), 200
-
-
-@fluctuations.route('/lbp-to-usd', methods=['GET'], strict_slashes=False)
-def get_exchangeRateFluctuationsLBP():
-    args = request.args
-    startYear = int(args.get('startYear'))
-    startMonth = int(args.get('startMonth'))
-    startDay = int(args.get('startDay'))
-    endYear = int(args.get('endYear'))
-    endMonth = int(args.get('endMonth'))
-    endDay = int(args.get('endDay'))
-
-    try:
-        START_DATE = datetime.datetime(startYear, startMonth, startDay)
-        END_DATE = datetime.datetime(endYear, endMonth, endDay)
-    except ValueError:
-        return jsonify({"error": "Invalid date format. Please use yyyy-mm-dd."}), 400
-
-    fluctuations = []
-
-    current_date = START_DATE
-    while current_date <= END_DATE:
-        start_of_period = current_date
-        end_of_period = current_date + \
-            datetime.timedelta(hours=23, minutes=59, seconds=59)
-        usd_to_lbp_rate, lbp_to_usd_rate = getExchangeRates(
-            start_of_period, end_of_period)
-        start_of_period = start_of_period.strftime("%Y-%m-%d")
-        end_of_period = end_of_period.strftime("%Y-%m-%d")
-        if (usd_to_lbp_rate == 0):
-            usd_to_lbp_rate = "No Data Available"
-        if (lbp_to_usd_rate == 0):
-            lbp_to_usd_rate = "No Data Available"
-
-        FluctuationResponseLbpToUsd = fluctuationResponseLbpToUsd(
-            start_of_period, end_of_period, lbp_to_usd_rate)
-        fluctuations.append(FluctuationResponseLbpToUsd.serialize())
+        FluctuationResponse = FluctuationResponse(
+            start_of_period, usd_to_lbp_rate, lbp_to_usd_rate)
+        fluctuations.append(FluctuationResponse.serialize())
         current_date += datetime.timedelta(days=1)
 
     return jsonify(fluctuations), 200
--- services/fluctuationResponse.py
+++ services/fluctuationResponse.py
@@ -1,21 +1,11 @@
 import json
 
 
-class fluctuationResponseUsdToLbp:
-    def __init__(self, StartDate, EndDate, usdToLbpRate):
+class FluctuationResponse:
+    def __init__(self, StartDate, usdToLbpRate, lbpToUsdRate):
         self.StartDate = StartDate
-        self.EndDate = EndDate
         self.usdToLbpRate = usdToLbpRate
-
-    def serialize(self):
-        return {"StartDate": self.StartDate, "EndDate": self.EndDate, "usdToLbpRate": self.usdToLbpRate}
-
-
-class fluctuationResponseLbpToUsd:
-    def __init__(self, StartDate, EndDate, lbpToUsdRate):
-        self.StartDate = StartDate
-        self.EndDate = EndDate
         self.lbpToUsdRate = lbpToUsdRate
 
     def serialize(self):
-        return {"StartDate": self.StartDate, "EndDate": self.EndDate, "lbpToUsdRate": self.lbpToUsdRate}
+        return {"Date": self.StartDate, "usdToLbpRate": self.usdToLbpRate, "lbpToUsdRate": self.lbpToUsdRate}
